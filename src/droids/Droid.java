141b4a696889dd11442b83eafe573cb5f8b0745c
OlgaKhytriak <KhytriakOlga@gmail.com>
2017-10-12 17:32:26 +0300

    Method move is added in the class SimpleRepairDroid

--- src/droids/Droid.java
+++ src/droids/Droid.java
@@ -6,10 +6,13 @@ package droids;
  */
 public abstract class Droid {
 	private Integer power;
+	private Integer health;
+	private Integer energy;
+	private byte positionX;
+	private byte positionY;
 	
-	abstract public  void  walk();
-
-	/**
+	
+		/**
 	 * @return
 	 */
 	public Integer getPower() {
@@ -26,4 +29,38 @@ public abstract class Droid {
 	public Droid() {
 
 	}
+
+	public Integer getHealth() {
+		return health;
+	}
+
+	public void setHealth(Integer health) {
+		this.health = health;
+	}
+
+	public Integer getEnergy() {
+		return energy;
+	}
+
+	public void setEnergy(Integer energy) {
+		this.energy = energy;
+	}
+
+	public Byte getPositionX() {
+		return positionX;
+	}
+
+	public void setPositionX(Byte positionX) {
+		this.positionX = positionX;
+	}
+
+	public Byte getPositionY() {
+		return positionY;
+	}
+
+	public void setPositionY(Byte positionY) {
+		this.positionY = positionY;
+	}
+
+	abstract public void walk(byte x, byte y);
 }
--- src/droids/SimpleRepairDroid.java
+++ src/droids/SimpleRepairDroid.java
@@ -16,9 +16,24 @@ public class SimpleRepairDroid extends Droid implements RepairDroid {
 		
 	}
 
+	/* 
+	 * If your energy >0 you can walk. When you reach desired position your energy reduces on 1 point.
+	 * If your energy =0 you cant walk.
+	 * Parameters are the coordinates x, y of the game's field into which the droid shoul be moved  
+	 */
 	@Override
-	public void walk() {
-		// TODO Auto-generated method stub
+	public void walk(byte x, byte y) {
+		if (this.getEnergy()>0){
+			this.setPositionX(x);
+			this.setPositionY(y);
+			this.setEnergy(this.getEnergy()-1);}
+			else {
+				System.out.println("You dont have enough energy to w");
+				return;
+			}
 		
 	}
+
+	
+	
 }
